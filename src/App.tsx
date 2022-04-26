a5702a22057c9079f9429b5b32f1fedc8aa306e3
Myos <68583272+willy14620@users.noreply.github.com>
2022-04-26 20:27:17 +0800

    fix: dashboard index page route

--- src/App.tsx
+++ src/App.tsx
@@ -12,6 +12,7 @@ function App() {
       <Routes>
         <Route path="/tasks" element={<HomePage />} />
         <Route path="/tasks/dashboard" element={<DashboardPage />}>
+          <Route index element={<Content title="All" />} />
           <Route path="all" element={<Content title="All" />} />
           <Route path="completed" element={<Content title="Completed" />} />
           <Route path="in-progress" element={<Content title="In Progress" />} />
