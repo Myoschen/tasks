868cc8184a1037f45c5c5e0cf006839ecdfa1512
Myos <68583272+willy14620@users.noreply.github.com>
2022-04-27 22:35:25 +0800

    feat: add user login and signup interface

--- src/types.d.ts
+++ src/types.d.ts
@@ -3,3 +3,14 @@ interface Task {
   content: string;
   completed: boolean;
 }
+
+interface UserLogin {
+  email: string;
+  password: string;
+}
+
+interface UserSignUp {
+  username: string;
+  email: string;
+  password: string;
+}
