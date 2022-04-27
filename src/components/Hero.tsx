670f3209636e7cc26ead70345bd38cb57b4923c8
Myos <68583272+willy14620@users.noreply.github.com>
2022-04-27 22:31:28 +0800

    refactor: adjust the font size of the title

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -10,7 +10,7 @@ function Hero({ title, children }: HeroProps) {
     <div className="flex items-center relative h1 h-[calc(100vh-4.5rem)] bg-hero-pattern bg-no-repeat bg-cover bg-center">
       <div className="absolute w-full h-full pointer-events-none backdrop-blur-sm" />
       <div className="container z-10 flex flex-col items-center justify-between mx-auto gap-y-12">
-        <h1 className="text-6xl font-bold">
+        <h1 className="mb-6 text-4xl font-bold md:text-6xl">
           {title}
         </h1>
         {children}
