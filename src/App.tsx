f933a14c582fe11f9b62f2b7b3509a4f10d7921f
Myos <68583272+willy14620@users.noreply.github.com>
2022-04-29 00:16:23 +0800

    refactor: redefine routing paths

--- src/App.tsx
+++ src/App.tsx
@@ -2,23 +2,33 @@ import React from 'react';
 import {
   BrowserRouter as Router, Routes, Route,
 } from 'react-router-dom';
+import { Provider } from 'react-redux';
+
+// Components
 import Content from './components/Content';
+
+// Pages
 import DashboardPage from './pages/DashboardPage';
 import HomePage from './pages/HomePage';
 
+// Store
+import store from './slices/store';
+
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/tasks" element={<HomePage />} />
-        <Route path="/tasks/dashboard" element={<DashboardPage />}>
-          <Route index element={<Content title="All" />} />
-          <Route path="all" element={<Content title="All" />} />
-          <Route path="completed" element={<Content title="Completed" />} />
-          <Route path="in-progress" element={<Content title="In Progress" />} />
-        </Route>
-      </Routes>
-    </Router>
+    <Provider store={store}>
+      <Router>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/dashboard" element={<DashboardPage />}>
+            <Route index element={<Content title="All" />} />
+            <Route path="all" element={<Content title="All" />} />
+            <Route path="completed" element={<Content title="Completed" />} />
+            <Route path="in-progress" element={<Content title="In Progress" />} />
+          </Route>
+        </Routes>
+      </Router>
+    </Provider>
   );
 }
 
