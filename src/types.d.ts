30a66ff760f4a4fecbbb3f001b4212e7e95f198f
Myos <68583272+willy14620@users.noreply.github.com>
2022-04-29 00:11:39 +0800

    add: some global types and utilities

--- src/types.d.ts
+++ src/types.d.ts
@@ -4,13 +4,21 @@ interface Task {
   completed: boolean;
 }
 
-interface UserLogin {
-  email: string;
-  password: string;
-}
-
-interface UserSignUp {
+interface IUser {
   username: string;
   email: string;
   password: string;
 }
+
+declare function assertIsError(error: unknown): asserts error is Error {
+  // if you have nodejs assert:
+  // assert(error instanceof Error);
+  // otherwise
+  if (!(error instanceof Error)) {
+    throw error;
+  }
+}
+
+type RemoveReadOnlyProperty<T> = {
+  -readonly [K in keyof T]: T[K];
+};
